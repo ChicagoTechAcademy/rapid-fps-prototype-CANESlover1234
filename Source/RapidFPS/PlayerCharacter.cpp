edf8890f4c2e60c66d38c550ed102a04f17fd043
CANESlover1234 <jdlagunas@chicagotechacademy.org>
2026-05-12 11:44:39 -0500

    video 5 done

--- Source/RapidFPS/PlayerCharacter.cpp
+++ Source/RapidFPS/PlayerCharacter.cpp
@@ -41,10 +41,13 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent)) {
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Move);
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Jump);
 		EnhancedInputComponent->BindAction(ShootAction, ETriggerEvent::Started, this, &APlayerCharacter::Shoot);
 		EnhancedInputComponent->BindAction(ReloadAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Reload);
 		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Look);
+
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+
 	}
 }
 
@@ -76,7 +79,7 @@ void APlayerCharacter::Look(const FInputActionValue& Value)
 }	
 void APlayerCharacter::Jump(const FInputActionValue& Value)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Your Pressed a jump button"));
+	//UE_LOG(LogTemp, Warning, TEXT("Your Pressed a jump button"));
 }
 
 
