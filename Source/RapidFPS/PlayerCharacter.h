d42c98387539da7cfbf976e01231b2747659e1f9
CANESlover1234 <jdlagunas@chicagotechacademy.org>
2026-05-12 11:17:03 -0500

    Finished video 4

--- Source/RapidFPS/PlayerCharacter.h
+++ Source/RapidFPS/PlayerCharacter.h
@@ -44,6 +44,16 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
 	FVector MuzzleOffset;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ammo")
+	int maxAmmo = 5;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ammo")
+	int currentAmmo;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ammo")
+	int storedAmmo;
+
+	void  DecreaseAmmo(int ammoSpent);
 
 	// In your class definition:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
